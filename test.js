77047a5158a2d0c0e54c2a1147c9c6cbd5ba692e
juhasz-szabolcs <juhasz.szabolcs@szbi-pg.hu>
2026-01-08 12:06:16 +0100

    javascript gombok

---
+++ test.js
@@ -0,0 +1,12 @@
+console.log("Hello World!!!")
+
+function kep_beilleszt() {
+    document.getElementById("kep_helye").innerHTML = "<img src='https://www.mafab.hu/static/2026t/2/15/43753_1767449877.8365.jpg' alt='' width='350px'>"
+}
+
+function kep_elrejt() {
+    document.getElementById("kep_helye").innerHTML = ""
+}
+
+console.warn("Ez egy figyelmeztetés")
+console.error("Ez egy hibaüzenet")
